f15c2e946fb638e6aa5494ae10d95f7ab5737638
Tim Bryant <timothybryant3@gmail.com>
2024-12-25 17:40:54 -0500

    feat: Add script to sync backups to NAS

---
+++ Productivity/Active/hoarder/backups_to_nas.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Variables
+LOCAL_BACKUP_DIR="$HOME/ghq/github.com/timmyb824/docker-apps/Productivity/Active/hoarder/backups" 
+NAS_BACKUP_DIR="/mnt/bryantnas/remote_file_backups/podman-01/hoarder_backups"              # Path to the NAS backup directory
+LOG_FILE="/var/log/hoarder_backups_to_nas.log"                 # Log file for the script
+
+if ! [ -d "$NAS_BACKUP_DIR" ] || ! ls "$NAS_BACKUP_DIR" &>/dev/null; then
+    echo "$(date): NAS is not accessible. Exiting." | tee -a "$LOG_FILE"
+    exit 1
+fi
+
+# Sync backups using rsync
+rsync -avh --delete "$LOCAL_BACKUP_DIR" "$NAS_BACKUP_DIR" >> "$LOG_FILE" 2>&1
+
+# Log success
+if [ $? -eq 0 ]; then
+    echo "$(date): Backups synced successfully to $NAS_BACKUP_DIR." | tee -a "$LOG_FILE"
+else
+    echo "$(date): Error syncing backups to $NAS_BACKUP_DIR." | tee -a "$LOG_FILE"
+fi
