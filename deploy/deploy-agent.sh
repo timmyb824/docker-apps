35d8b8d5921492300bd88fbb7a66e4b14d75216b
Tim Bryant <timothybryant3@gmail.com>
2025-11-19 12:41:51 -0500

    fix: Update decryption command to use correct input type

--- deploy/deploy-agent.sh
+++ deploy/deploy-agent.sh
@@ -80,8 +80,7 @@ for app_dir in "${changed_app_dirs[@]}"; do
 
 	# 1) decrypt env
 	if [ -f "$app_dir/.app.env" ]; then
-		sops --decrypt --age "$(cat $SOPS_AGE_KEY_FILE | grep -oP "public key: \K(.*)")" -i "$app_dir/.app.env" > "$app_dir/.env"
-
+		sops --decrypt --input-type dotenv "$app_dir/.app.env" > "$app_dir/.env"
 	fi
 
 	# 2) redeploy
